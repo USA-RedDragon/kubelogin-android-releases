24c24c56a5f15f36be836bce52e1a60b8a2491d8
Jacob McSwain <jacob.a.mcswain@gmail.com>
2023-10-01 20:22:47 -0500

    renovate: another regex fix

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # renovate: datasource=github-tags depName=int128/kubelogin
 KUBELOGIN_VERSION=v1.27.0
-# renovate: datasource=github-tags depName=android/ndk versioning=loose
+# renovate: datasource=github-tags depName=android/ndk versioning=semver-coerced
 NDK_VERSION=r26
 
 # Clone the repo
